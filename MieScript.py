a0fa0bf96b2bfad1708fb45f28c2805a783dc546
ProGamerGo1234 <martin.takac47@gmail.com>
2024-10-21 15:55:17 +0400

    MieScript.py

--- MieScript.py
+++ MieScript.py
@@ -1,1 +1,6 @@
 print('Welcome To MieScript')
+print('This is an open source code, so you can edit it!')
+username = input('What is your name? ')
+print('Welcome to the terminal')
+while True:
+terminnal = input('/')
